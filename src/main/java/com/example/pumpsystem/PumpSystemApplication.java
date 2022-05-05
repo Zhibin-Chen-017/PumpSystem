c1219cc417c8fd9f4e7ba22b8bbd0968e4644a2d
ZhibinChen <1179331904@qq.com>
2022-05-05 22:45:21 +0800

    Initial commit

---
+++ src/main/java/com/example/pumpsystem/PumpSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.pumpsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PumpSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PumpSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pumpsystem/PumpSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pumpsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PumpSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
